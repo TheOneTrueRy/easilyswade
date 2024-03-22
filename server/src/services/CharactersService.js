13dacea7409ef1c5c9b449c0b73a9f479a9dcdbd
Ryan Wittman <OfficiallyRy@outlook.com>
2024-03-22 10:05:50 -0700

    Wrote getAllCharacters, getCharacterById, and updateCharacter

--- server/src/services/CharactersService.js
+++ server/src/services/CharactersService.js
@@ -1,5 +1,28 @@
-class CharactersService {
+import { dbContext } from "../db/DbContext.js";
+import { Forbidden } from "../utils/Errors.js";
 
+class CharactersService {
+  async getAllCharacters() {
+    let characters = await dbContext.Character.find()
+      .populate('creator')
+    return characters
+  }
+
+  async getCharacterById(characterId) {
+    let character = await dbContext.Character.findById(characterId)
+      .populate('creator')
+    return character
+  }
+
+  async updateCharacter(characterId, characterData, requestorId) {
+    let character = await this.getCharacterById(characterId)
+    if (requestorId == character.creatorId) {
+      await dbContext.Character.findByIdAndUpdate(characterId, characterData)
+      return character
+    } else {
+      throw new Forbidden("That's not your character to update!")
+    }
+  }
 };
 
 export const charactersService = new CharactersService();
