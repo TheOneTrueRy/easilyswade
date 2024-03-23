5c835567ea883db58e76bdd6f46c55e278eb27f2
Ryan Wittman <OfficiallyRy@outlook.com>
2024-03-22 22:28:49 -0600

    Wrote GetAllHindrances & GetHindranceById

--- server/src/controllers/HindrancesController.js
+++ server/src/controllers/HindrancesController.js
@@ -1,9 +1,30 @@
+import { hindrancesService } from "../services/HindrancesService.js";
 import BaseController from "../utils/BaseController.js";
 
 export class HindrancesController extends BaseController {
   constructor() {
     super('api/hindrances')
     this.router
+      .get('')
+      .get('/:hindranceId')
+  }
 
+  async getAllHindrances(req, res, next) {
+    try {
+      const hindrances = await hindrancesService.getAllHindrances()
+      res.send(hindrances)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getHidnranceById(req, res, next) {
+    try {
+      const hindranceId = req.params.hindranceId
+      const hindrance = await hindrancesService.getHindranceById(hindranceId)
+      res.send(hindrance)
+    } catch (error) {
+      next(error)
+    }
   }
 }
--- server/src/services/HindrancesService.js
+++ server/src/services/HindrancesService.js
@@ -1,4 +1,15 @@
+import { dbContext } from "../db/DbContext.js";
+
 class HindrancesService {
+  async getAllHindrances() {
+    let hindrances = await dbContext.Hindrance.find()
+    return hindrances
+  }
+
+  async getHindranceById(hindranceId) {
+    let hindrance = await dbContext.Hindrance.findById(hindranceId)
+    return hindrance
+  }
 
 };
 
