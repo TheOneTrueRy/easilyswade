b1c1aed999f717b7213b01b40849c5376f1b13ef
Ryan Wittman <OfficiallyRy@outlook.com>
2024-03-22 23:02:32 -0600

    Wrote Edge Model? Think this is the route I'm going?

--- server/src/models/Edge.js
+++ server/src/models/Edge.js
@@ -5,6 +5,11 @@ const Schema = mongoose.Schema;
 
 export const EdgeSchema = new Schema(
   {
-
+    creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Profile' },
+    characterId: { type: Schema.Types.ObjectId, required: true, ref: 'Character' },
+    name: { type: String, required: true, maxLength: 80, minLength: 2 },
+    description: { type: String, maxLength: 3000 },
+    rank: { type: String, enum: ['Novice', 'Seasoned', 'Veteran', 'Heroic', 'Legendary'], default: 'Novice' },
+    additionalRequirements: { type: String, maxLength: 300 }
   }, defaultSchemaOptions
 )
