6dc4ac0b3c50968f1de4de0db6e863f5999ac2a6
Ryan Wittman <OfficiallyRy@outlook.com>
2024-03-24 02:29:25 -0600

    Gave PartiesController its CRUD methods and paths, no functions tho I'm going to sleep

--- server/src/controllers/PartiesController.js
+++ server/src/controllers/PartiesController.js
@@ -1,9 +1,14 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
 import BaseController from "../utils/BaseController.js";
 
 export class PartiesController extends BaseController {
   constructor() {
     super('api/parties')
     this.router
-
+      .get('')
+      .get('/:partyId')
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('')
+      .put('/:partyId')
   }
 }
