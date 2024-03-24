f827e917c4df71db310430df8e6b763a78c02921
Ryan Wittman <OfficiallyRy@outlook.com>
2024-03-24 00:26:59 -0600

    creatorId and characterId taken off Edge as they no longer needed, wrote Power Schema

--- server/src/models/Edge.js
+++ server/src/models/Edge.js
@@ -5,8 +5,6 @@ const Schema = mongoose.Schema;
 
 export const EdgeSchema = new Schema(
   {
-    creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Profile' },
-    characterId: { type: Schema.Types.ObjectId, required: true, ref: 'Character' },
     name: { type: String, required: true, maxLength: 80, minLength: 2 },
     description: { type: String, maxLength: 3000 },
     rank: { type: String, enum: ['Novice', 'Seasoned', 'Veteran', 'Heroic', 'Legendary'], default: 'Novice' },
