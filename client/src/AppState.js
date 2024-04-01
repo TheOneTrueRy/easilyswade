3d89dcc34a1e51b0ca27beb7b992970dda483f09
Ry <OfficiallyRy@outlook.com>
2024-04-01 15:25:45 -0400

    Added (probably) all necessary variables in AppState

--- client/src/AppState.js
+++ client/src/AppState.js
@@ -3,6 +3,17 @@ import { reactive } from 'vue'
 // NOTE AppState is a reactive object to contain app level data
 export const AppState = reactive({
   user: {},
-  /** @type {import('./models/Account.js').Account} */
-  account: {}
+  account: {},
+
+  profile: null,
+
+  character: null,
+  characters: [],
+
+  party: null,
+  dungeonMasters: [],
+
+  art: null || [],
+
+  story: null
 })
