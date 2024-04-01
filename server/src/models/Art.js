8c97d5fe6b459ff6299d2273932f240b90080d80
Ry <OfficiallyRy@outlook.com>
2024-04-01 10:27:56 -0400

    added Supabase module as I'm going to try using it with Node still instead of C#, filled out Art schema

--- server/src/models/Art.js
+++ server/src/models/Art.js
@@ -5,6 +5,15 @@ const Schema = mongoose.Schema;
 
 export const ArtSchema = new Schema(
   {
-    creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Profile' }
+    creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Profile' },
+    title: { type: String, required: true, minLength: 2, maxLength: 40 },
+    description: { type: String, maxLength: 400 }
   }, defaultSchemaOptions
 )
+
+ArtSchema.virtual('creator', {
+  localField: 'creatorId',
+  foreignField: '_id',
+  ref: 'Profile',
+  justOne: true
+})
