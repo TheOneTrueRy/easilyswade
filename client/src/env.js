58196a87114731fc2c8f5bdce65191d6d08b0f39
Ry <OfficiallyRy@outlook.com>
2024-04-01 11:19:05 -0400

    added supabase Url and Public key to client env

--- client/src/env.js
+++ client/src/env.js
@@ -4,3 +4,5 @@ export const useSockets = false
 export const domain = 'pandaverse.us.auth0.com'
 export const clientId = 'kiY2oBjxNaL9q1NKQMyWZvXcCGJ7rJIk'
 export const audience = 'https://panda-verse.com'
+export const supabaseUrl = 'https://udixydcfgbzwwnvpwubp.supabase.co'
+export const supabasePublicKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkaXh5ZGNmZ2J6d3dudnB3dWJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEyMzM2NTksImV4cCI6MjAyNjgwOTY1OX0.E8w99aSc0yBVD-QoFU_JDkxhfQaXxdiHDB_ScOb5HOE'
