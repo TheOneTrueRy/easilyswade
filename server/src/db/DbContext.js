072f1cc8e44223f4d88c975fa1e2a7e38411ec6f
Ry <OfficiallyRy@outlook.com>
2024-04-03 01:29:45 -0400

    Added JoinRequest Schema and added both new schema to DbContext

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -4,6 +4,8 @@ import { CharacterSchema } from "../models/Character.js";
 import { PartySchema } from "../models/Party.js";
 import { StorySchema } from "../models/Story.js";
 import { ArtSchema } from "../models/Art.js";
+import { PartyInviteSchema } from "../models/PartyInvite.js";
+import { JoinRequestSchema } from "../models/JoinRequest.js";
 
 class DbContext {
   Account = mongoose.model('Account', AccountSchema);
@@ -11,6 +13,8 @@ class DbContext {
   Art = mongoose.model('Art', ArtSchema);
   Party = mongoose.model('Party', PartySchema);
   Story = mongoose.model('Story', StorySchema);
+  PartyInvite = mongoose.model('PartyInvite', PartyInviteSchema);
+  JoinRequest = mongoose.model('JoinRequest', JoinRequestSchema);
 }
 
 export const dbContext = new DbContext()
---
+++ server/src/models/JoinRequest.js
@@ -0,0 +1,35 @@
+import { Schema } from "mongoose";
+import { defaultSchemaOptions } from "../db/Constants.js";
+
+export const JoinRequestSchema = new Schema(
+  {
+    creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Account' },
+    partyId: { type: Schema.Types.ObjectId, required: true, ref: 'Party' },
+    characterId: { type: Schema.Types.ObjectId, required: true, ref: 'Character' },
+    seen: { type: Boolean, required: true, default: false }
+  }, defaultSchemaOptions
+)
+
+JoinRequestSchema.virtual('creator', {
+  localField: 'creatorId',
+  foreignField: '_id',
+  ref: 'Account',
+  justOne: true
+}
+)
+
+JoinRequestSchema.virtual('party', {
+  localField: 'partyId',
+  foreignField: '_id',
+  ref: 'Party',
+  justOne: true
+}
+)
+
+JoinRequestSchema.virtual('character', {
+  localField: 'characterId',
+  foreignField: '_id',
+  ref: 'Character',
+  justOne: true
+}
+)
