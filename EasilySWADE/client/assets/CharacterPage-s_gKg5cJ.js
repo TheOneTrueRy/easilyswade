843fe10946390a7a0bc7c2a79005c983b25dc21a
Ry <OfficiallyRy@outlook.com>
2024-05-28 23:02:45 -0400

    OH MY STARS IT'S BECAUSE I DIDN'T COMMIT ISN'T IT

---
+++ EasilySWADE/client/assets/CharacterPage-s_gKg5cJ.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o}from"./index-btuYbHwl.js";const c={setup(){return{}}},s={class:"component"};function r(n,a,_,p,d,f){return t(),o("div",s)}const u=e(c,[["render",r]]);export{u as default};
--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'docs',
+    outDir: '../EasilySWADE/client',
     sourcemap: false
   },
   server: {
