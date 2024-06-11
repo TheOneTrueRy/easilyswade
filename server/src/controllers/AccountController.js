933330cff7ccca953b4fbad430412ec9c14acead
Ry <OfficiallyRy@outlook.com>
2024-06-11 19:46:00 -0400

    Goodbye World!

--- server/src/controllers/AccountController.js
+++ server/src/controllers/AccountController.js
@@ -14,7 +14,6 @@ export class AccountController extends BaseController {
   async getUserAccount(req, res, next) {
     try {
       const account = await accountService.getAccount(req.userInfo)
-      console.log('Hello World!')
       res.send(account)
     } catch (error) {
       next(error)
