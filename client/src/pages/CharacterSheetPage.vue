0f71a084f3b95c53a971552fdf1053b73dac1802
Ry <OfficiallyRy@outlook.com>
2024-07-01 22:06:06 -0400

    Consistency across character sheet versions for user conditionals

--- client/src/pages/CharacterSheetPage.vue
+++ client/src/pages/CharacterSheetPage.vue
@@ -18,7 +18,7 @@
       <div class="row mt-1">
         <div class="col-9 pb-2">
           <div class="row border-bottom ms-1">
-            <div class="col-6 ps-0">
+            <div class="col-6 ps-0 pb-2">
               <div class="row px-2">
                 <div class="col-12 text-center">
                   <span class="fs-3 fw-bold text-danger">
@@ -757,11 +757,11 @@
     </form>
   </div>
   <div v-else class="container-fluid p-3">
-    <div class="row mt-1 mb-4">
-      <div class="col-9">
-        <div class="row">
-          <div class="col-6">
-            <div class="row pe-1 ms-1">
+    <div class="row mt-1">
+      <div class="col-9 pb-2">
+        <div class="row border-bottom ms-1">
+          <div class="col-6 ps-0 pb-2">
+            <div class="row px-2">
               <div class="col-12 text-center">
                 <span class="fs-3 fw-bold text-danger">
                   ATTRIBUTES
@@ -983,7 +983,7 @@
                 </div>
               </div>
             </div>
-            <div class="row pe-2 ms-1 mt-4 border-top">
+            <div class="row px-2 mt-4">
               <div class="col-12 text-center">
                 <span class="fs-3 fw-bold text-danger">
                   SKILLS
@@ -1038,7 +1038,7 @@
               </div>
             </div>
           </div>
-          <div class="col-6 border-start px-4">
+          <div class="col-6 border-start px-4 pb-2">
             <div class="row mt-4">
               <div class="col-12 d-flex align-items-end px-0"
                 :class="theme == 'light' ? 'border-dark' : 'border-light'">
